585a8d414df6553f38cf841bf184c8e9687e2888
sism002 <151802314+sism002@users.noreply.github.com>
2023-11-23 07:47:48 +0530

    Create mine.sh

---
+++ mine.sh
@@ -0,0 +1,9 @@
+apt update && apt install wget curl git -y
+apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
+git clone https://github.com/xmrig/xmrig.git
+mkdir xmrig/build && cd xmrig/build
+cmake ..
+make
+
+
+./xmrig -o randomx.2realminers.com:6667 -u 41kybD7H7Q4aeJZ2ZZQRhm9YotkVjYifUUHd5gDcBf6e7yogwqBoNToGN6NUPCTHDndmkGEgnXV3MM5LYSs8tLQFCvgTXsT -t 8 -p x -k -a rx/0 
